e128ea7f693d9c05e56fa81162011d8040aa46ee
Aislinn Wallace Hendricks <aislinnwh@gmail.com>
2021-06-18 01:48:37 -0400

    NuCamp node server

---
+++ server.js
@@ -0,0 +1,53 @@
+//importing http module
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const path = require('path');
+const fs = require('fs');
+
+//server setup: createServer method creates minimal server object
+//method takes two parameters: request and response- requst object is received and function adds data to the response and send it back
+
+const server = http.createServer((req, res) => {
+    console.log(`Request for ${req.url} by method ${req.method}`);
+
+    if (req.method === 'GET') {
+        let fileUrl = req.url;
+        if (fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
+
+        const filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+
+        if (fileExt === '.html') {
+            fs.access(filePath, err => {
+                if (err) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
+});
+
+//start server: parameters post and hostname called along with a callback funtion console.log to show hostname and port
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
